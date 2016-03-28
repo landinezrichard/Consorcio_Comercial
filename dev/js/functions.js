2dca972d894b35620ac8588323ebd2b3111d0e5b
Ricardo <landinezrichard@gmail.com>
2016-03-28 13:02:02 -0500

    agregado carrusel otros proyectos

--- dev/js/functions.js
+++ dev/js/functions.js
@@ -21,6 +21,20 @@ $(document).ready(function() {
 		}
 	});
 
+	/*Slider Otros Proyectos*/
+	$('.Index-itemSlideCarousel.owl-carousel').owlCarousel({		
+		nav: false,
+		autoPlay: false,
+		slideSpeed: 1000,
+		pagination: true,
+		loop:true,
+		responsive: {
+			0:{
+				items:1
+			}
+		}
+	});
+
 	/*Modificar los controles del Owl*/
 	$.each($('.owl-prev'),function(elemento){
 			$(this).html(arrow_izq);
